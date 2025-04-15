59d1631f34efa51b4d13822c2e15399c21a4e76a
Shrawani Tati <tatishravani@gmail.com>
2025-04-16 00:00:16 +0530

    Added  profilepage

--- frontend/src/routes/Routers.jsx
+++ frontend/src/routes/Routers.jsx
@@ -6,9 +6,10 @@ import Signup from "../pages/Signup";
 import Contact from "../pages/Contact";
 import Doctors from "../pages/Doctors/Doctors";
 import DoctorDetails from "../pages/Doctors/DoctorDetails";
-import MyAccount from "../../Dasboard/user-account/MyAccount";
-import Dashboard from "../../Dasboard/doctor-account/Dashboard";
+import MyAccount from "../Dashboard/user-account/MyAccount";
+import Dashboard from "../Dashboard/doctor-account/Dashboard";
 import { Routes, Route } from "react-router-dom";
+import ProtectedRoute from "./ProtectedRoute";
 const Routers = () => {
   return (
     <Routes>
@@ -20,8 +21,8 @@ const Routers = () => {
       <Route path="/register" element={<Signup />} />
       <Route path="/contact" element={<Contact />} />
       <Route path="/services" element={<Services />} />
-      <Route path="/users/profile/me" element={<MyAccount />} />
-      <Route path="/doctors/profile/me" element={<Dashboard />} />
+      <Route path="/users/profile/me" element={<ProtectedRoute allowedRoles ={ ['patient']} ><MyAccount /></ProtectedRoute> } />
+      <Route path="/doctors/profile/me" element={<ProtectedRoute allowedRoles ={ ['doctor']}><Dashboard /></ProtectedRoute>} />
     </Routes>
   );
 };
